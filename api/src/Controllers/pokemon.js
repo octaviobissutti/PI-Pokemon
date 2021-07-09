6a76f949c54e4bce738914a16d097de4e8abe34d
octaviobissutti <octavio_bissutti@hotmail.com>
2021-07-09 16:27:46 -0300

    Back terminado

--- api/src/Controllers/pokemon.js
+++ api/src/Controllers/pokemon.js
@@ -6,60 +6,73 @@ const db = require("../db");
 async function getAllPokemons(req, res) {
     let type;
     let name = req.query.name;
+    
     if(name) { //No me busca el pokemon creado, si los de la api.
-        var lower = name.toLowerCase();
         try {
-            let api = await axios.get(`${URL}${POKEMON}/${lower}`); 
-            if(api.data.types.length === 1) {
-                type = api.data.types[0].type.name;
-            } else {
-                type = api.data.types[0].type.name + " " + api.data.types[1].type.name;
-            }
-            console.log(type);
-            var pokeApi = {
-                name: api.data.name.charAt(0).toUpperCase() + api.data.name.slice(1),
-                id: api.data.id,
-                image: api.data.sprites.other.dream_world.front_default,
-                types: type,
-                height: api.data.height,
-                weight: api.data.weight,
-                hp: api.data.stats[0].base_stat,
-                attack: api.data.stats[1].base_stat,
-                defense: api.data.stats[2].base_stat,
-                speed: api.data.stats[5].base_stat
-            } 
-        } catch (error) {
+            // console.log('NAMEEEEEEEEE: ', name) //Dani
+            var lower = name.toLowerCase();
             const dataBase = await Pokemon.findOne({
                 where:{
-                    name: lower
+                    name: lower.charAt(0).toUpperCase() + lower.slice(1)
                 },
                 include: Type
             })
-            if(!dataBase) {
-                return res.status(404).send({message: 'Bad Request'})
-            }
-            if(dataBase.types.length === 1) {
-                type = dataBase.types[0].name;
+            if(dataBase) {
+                // console.log('EL NOMBREEEEE: ', name);
+                // console.log('DATAA BASEE: ', dataBase); //NULL
+
+                if(dataBase.types.length === 1) {
+                    type = dataBase.types[0].name;
+                } else {
+                    type = dataBase.types[0].name + " " + dataBase.types[1].name;
+                }
+                var pokeDb ={
+                    name : dataBase.name.charAt(0).toUpperCase() + dataBase.name.slice(1),
+                    id: dataBase.id,
+                    types: type,
+                    height: dataBase.height,
+                    weight: dataBase.weight,
+                    image: "https://i.playboard.app/p/AAUvwngrNsz0VgH-cA-girh64i7q941e6mxWACzbtr7a0A/default.jpg",
+                    hp: dataBase.hp,
+                    attack: dataBase.attack,
+                    defense: dataBase.defense,
+                    speed: dataBase.speed
+      
+                } 
+                return res.send(pokeDb);
             } else {
-                type = dataBase.types[0].name + " " + dataBase.types[1].name;
+
+                let api = await axios.get(`${URL}${POKEMON}/${lower}`); 
+                // console.log('ENTRE APIIIIIIIIIIIIIIII; ');
+                if(api) {
+    
+                    // console.log('ENTRE API: ', api);
+                    if(api.data.types.length === 1) {
+                        type = api.data.types[0].type.name;
+                    } else {
+                        type = api.data.types[0].type.name + " " + api.data.types[1].type.name;
+                    }
+                
+                    var pokeApi = {
+                        name: api.data.name.charAt(0).toUpperCase() + api.data.name.slice(1),
+                        id: api.data.id,
+                        image: api.data.sprites.other.dream_world.front_default,
+                        types: type,
+                        height: api.data.height,
+                        weight: api.data.weight,
+                        hp: api.data.stats[0].base_stat,
+                        attack: api.data.stats[1].base_stat,
+                        defense: api.data.stats[2].base_stat,
+                        speed: api.data.stats[5].base_stat
+                    } 
+                    return res.send(pokeApi);
             }
-            var pokeDb ={
-                name : dataBase.name.charAt(0).toUpperCase() + dataBase.name.slice(1),
-                id: dataBase.id,
-                types: type,
-                height: dataBase.height,
-                weight: dataBase.weight,
-                image: "https://i.playboard.app/p/AAUvwngrNsz0VgH-cA-girh64i7q941e6mxWACzbtr7a0A/default.jpg",
-                hp: dataBase.hp,
-                attack: dataBase.attack,
-                defense: dataBase.defense,
-                speed: dataBase.speed
-  
-            } 
-            return res.send(pokeDb);
+            }
             
+        
+        } catch (error) {
+         return res.status(404).send({error: "Pokemon don´t found!! :("});
         }
-        return res.send(pokeApi);
   
     }  else {
         try {
@@ -117,68 +130,92 @@ async function getAllPokemons(req, res) {
 
 async function addPokemon(req, res) {   
   const { name, image, types, height, weight, hp, attack, defense, speed } =  req.body;
+//   console.log('BODY: ', req.body)
 
   if (!name) {
     return res.status(400).json({ error: "notNull Violation: It requires a valid name" });
-  } else {
-    const createPokemon = await Pokemon.create({
-      name: name,
-      image: image,
-      types: types, //No me muestra los types!
-      height: height,
-      weight: weight,
-      hp: hp,
-      attack: attack,
-      defense: defense,
-      speed: speed,
-    });
-    await createPokemon.setTypes(types); //set método de sequelize para relacionar las tablas. El set pisa los valores anteriores.
-    return res.json(createPokemon);
-  }
+  } //else {
+    try {
+        
+        const createPokemon = await Pokemon.create({
+          name: name,
+          image: image,
+          types: types, //No me muestra los types!
+          height: height,
+          weight: weight,
+          hp: hp,
+          attack: attack,
+          defense: defense,
+          speed: speed,
+        });
+        await createPokemon.addTypes(req.body.types, { through: 'pokemon_type'});
+        const pokeType = await Pokemon.findOne({
+            where: {name: req.body.name},
+            include: Type
+        })
+        return res.json(pokeType);
+    }  catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error')
+    }
+  //}
 }
+
+
+
+
+
+
 
 
 async function getPokemonById(req, res) {
     let type;
     let id = req.params.id; //No encuentra el pokemon creado, si los de la api.
     if(id) {
+    //  console.log('ID: ', id);   
         try {
-            let api = await axios.get(`${URL}${POKEMON}/${id}`);
-            if(!api) {
+            if(!id.includes('-')) {
+                var api = await axios.get(`${URL}${POKEMON}/${id}`);
+                // console.log('ENTRE API: ', api);
+                if(api?.data.types.length === 1) {
+                    type = api.data.types[0].type.name;
+                } else {
+                    type = api.data.types[0].type.name + " " + api.data.types[1].type.name;
+                }
+
+                var poke = {
+                    name: api.data.name,
+                    id: api.data.id,
+                    image: api.data.sprites.other.dream_world.front_default,
+                    types: type,
+                    height: api.data.height,
+                    weight: api.data.weight,
+                    hp: api.data.stats[0].base_stat,
+                    attack: api.data.stats[1].base_stat,
+                    defense: api.data.stats[2].base_stat,
+                    speed: api.data.stats[5].base_stat
+                } 
+                return res.json(poke);
+
+            } else { 
+            
                 const dataBase = await Pokemon.findOne({
                     where:{
                         id: id,
                     },
-                    include: Type
+                    include: [Type] 
                 })
+                // console.log('ENTRE DB: ', dataBase);
                 if(!dataBase) {
-                    return res.status(404).send({message: 'Bad Request'})
+                    return res.status(404).send({message: 'Pokemon don´t found'})
                 }
                 return res.send(dataBase);
                 
             }
-            if(api.data.types.length === 1) {
-                type = api.data.types[0].type.name;
-            } else {
-                type = api.data.types[0].type.name + " " + api.data.types[1].type.name;
-            }
-
-            var poke = {
-                name: api.data.name,
-                id: api.data.id,
-                image: api.data.sprites.other.dream_world.front_default,
-                types: type,
-                height: api.data.height,
-                weight: api.data.weight,
-                hp: api.data.stats[0].base_stat,
-                attack: api.data.stats[1].base_stat,
-                defense: api.data.stats[2].base_stat,
-                speed: api.data.stats[5].base_stat
-            } 
+           
         } catch (error) {
             return res.status(404).send({message: 'Bad Request'})
         }
-        return res.json(poke);
     }
 }
 
